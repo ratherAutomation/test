0dcffb93757373a774566c21c2fb4f92d5dd0a46
ratherAutomation <145386758+ratherAutomation@users.noreply.github.com>
2023-09-28 13:10:33 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+import dash
+from dash import dcc, html
+import plotly.express as px
+
+# Crear una aplicación Dash
+app = dash.Dash(__name__)
+
+# Definir el diseño del dashboard
+app.layout = html.Div([
+    html.H1("Dashboard de Prueba en Render"),
+    dcc.Graph(
+        id='grafico-de-barras',
+        figure=px.bar(
+            x=['A', 'B', 'C', 'D'],
+            y=[4, 7, 1, 2],
+            labels={'x': 'Categoría', 'y': 'Valor'},
+            title='Gráfico de Barras'
+        )
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True, host='0.0.0.0')
